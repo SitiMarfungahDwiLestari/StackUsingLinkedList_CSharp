a1562b73c9de59a2e9c74f0cff52e88d8274511a
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-02 16:38:48 +0700

    add class Stacks

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -12,8 +12,49 @@ namespace StackUsingLinkedList_CSharp
             next = n;
         }
     }
-    class Program
+    class Stacks
     {
+        Node top;
+        public Stacks()
+        {
+            top = null;
+        }
+        bool empty ()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + " pushed.");
+        }
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node in sequence
+        }
+        public void display()
+        {
+            Node tmp;
+            if (empty()) //If stack is empty
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                //Traverse the list from begginning till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
